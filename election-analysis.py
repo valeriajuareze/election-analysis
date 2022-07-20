e078f69ded5ecfbf764d5f3ba50f9bcc5fe44aec
valeriajuareze <108365182+valeriajuareze@users.noreply.github.com>
2022-07-20 12:24:36 -0500

    Adding election-analysis.py

---
+++ election-analysis.py
@@ -0,0 +1,33 @@
+# Create a variable called 'name' that holds a string
+name = "Valeria"
+
+# Create a variable called 'country' that holds a string
+country = "Mexico"
+
+# Create a variable called 'age' that holds an integer
+Age = 19
+
+# Create a variable called 'hourly_wage' that holds an integer
+Hourly_wage = 20 
+
+# Calculate the daily wage for the user
+Daily_wage = (Hourly_wage * 8)
+
+# Create a variable called 'satisfied' that holds a boolean
+Satisfied = False
+
+# Print out "Hello <name>!"
+print(f"Hello {name}")
+
+# Print out what country the user entered
+print (f"Country: {country}")
+
+# Print out the user's age
+print (f"Age: {Age}")
+
+# With an f-string, print out the daily wage that was calculated
+print (f"Daily Wage: {Daily_wage}")
+
+# With an f-string, print out whether the users were satisfied
+
+
